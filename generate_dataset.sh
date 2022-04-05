4214dc08eabf2d7e07e1f57bcf871bb64790aa4b
AbhishekM <31574739+MoitraAbhi@users.noreply.github.com>
2022-04-05 01:01:08 -0400

    Create generate_dataset.sh

---
+++ generate_dataset.sh
@@ -0,0 +1,12 @@
+dataset="cifar100"
+S_les_model="mobilenet_v2"
+out1=16
+task="${dataset}_sles_${S_les_model}"
+model_pth="../cifar100/clean_trained_${S_les_model}_ckpt.pth"
+train_set="../cifar100/adv_train_set_${task}"
+test_set="../cifar100/adv_test_set_${task}"
+saved_model="./models"
+log_pth="./logs"
+
+python adv_train_set_gen.py --type=$dataset --batch_size=200 --atype='pgd' --eps=0.04 --alpha=0.016 --steps=10 --path="./${dataset}" --model_pth=$model_pth --model=$S_les_model --task=$task --per=100
+python adv_dataset_gen.py --type=$dataset --batch_size=200 --atype='pgd' --eps=0.04 --alpha=0.016 --steps=10 --path="./${dataset}" --model_pth=$model_pth --model=$S_les_model --task=$task
