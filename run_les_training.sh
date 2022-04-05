318f8ba403aea5237f061d5caf2d185e7df37fc7
AbhishekM <31574739+MoitraAbhi@users.noreply.github.com>
2022-04-05 01:08:15 -0400

    Update run_les_training.sh

--- run_les_training.sh
+++ run_les_training.sh
@@ -10,6 +10,6 @@ test_set="../${dataset}/adv_test_set_${task}"
 saved_model="./models"
 log_pth="./logs"
 
-python phase1_gen_sois.py --type=$dataset --batch_size=200 --epochs=$epch --pgd_params=8_4_10 --train_pth=$train_set --test_pth=$test_set --save_pth=$saved_model --log_pth=$log_pth --task=$task --out=16,32,64 --stage=0
-python phase1_gen_sois.py --type=$dataset --batch_size=200 --epochs=$epch --pgd_params=8_4_10 --train_pth=$train_set --test_pth=$test_set --save_pth=$saved_model --log_pth=$log_pth --task=$task --out=16,32,64 --stage=1
-python phase1_gen_sois.py --type=$dataset --batch_size=200 --epochs=$epch --pgd_params=8_4_10 --train_pth=$train_set --test_pth=$test_set --save_pth=$saved_model --log_pth=$log_pth --task=$task --out=16,32,64 --stage=2
+python les_training.py --type=$dataset --batch_size=200 --epochs=$epch --pgd_params=8_4_10 --train_pth=$train_set --test_pth=$test_set --save_pth=$saved_model --log_pth=$log_pth --task=$task --out=16,32,64 --stage=0
+python les_training.py --type=$dataset --batch_size=200 --epochs=$epch --pgd_params=8_4_10 --train_pth=$train_set --test_pth=$test_set --save_pth=$saved_model --log_pth=$log_pth --task=$task --out=16,32,64 --stage=1
+python les_training.py --type=$dataset --batch_size=200 --epochs=$epch --pgd_params=8_4_10 --train_pth=$train_set --test_pth=$test_set --save_pth=$saved_model --log_pth=$log_pth --task=$task --out=16,32,64 --stage=2
